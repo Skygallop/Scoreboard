64566532e94327388b59a1dcdd21ae1284692068
Skygallop <lomasd73@gmail.com>
2022-12-12 19:57:32 -0800

    Base scoreboard done

--- Scoreboard/ScoreboardView.js
+++ Scoreboard/ScoreboardView.js
@@ -1,35 +1,37 @@
 export default class ScoreboardView {
-    constructor(root, playerOneName, playerTwoName, onControlButtonClick) {
-        this.root = root;
-        this.root.innerHTML = `
-        <div class="scoreboard">
-    <div class="scoreboard__name scoreboard__name--one">${playerOneName}</div>
-    <div class="scoreboard__name scoreboard__name--two">${playerTwoName}</div>
-    <div class="scoreboard__score" data-for-player="one">0</div>
-    <div class="scoreboard__score" data-for-player="two">0</div>
-    <div class="scoreboard__controls" data-for-player="one">
-    <button class="scoreboard__control-button">-</button>
-    <button class="scoreboard__control-button">+</button>
-</div>
-    <div class="scoreboard__controls" data-for-player="two">
-    <button class="scoreboard__control-button">-</button>
-    <button class="scoreboard__control-button">+</button>
- </div>
-</div>
-        `;
+	constructor(root, playerOneName, playerTwoName, onControlButtonClick) {
+		this.root = root;
+		this.root.innerHTML = `
+			<div class="scoreboard">
+				<div class="scoreboard__name scoreboard__name--one">${playerOneName}</div>
+				<div class="scoreboard__name scoreboard__name--two">${playerTwoName}</div>
+				<div class="scoreboard__score" data-for-player="one">0</div>
+				<div class="scoreboard__score" data-for-player="two">0</div>
+				<div class="scoreboard__controls" data-for-player="one">
+					<button class="scoreboard__control-button">-</button>
+					<button class="scoreboard__control-button">+</button>
+				</div>
+				<div class="scoreboard__controls" data-for-player="two">
+					<button class="scoreboard__control-button">-</button>
+					<button class="scoreboard__control-button">+</button>
+				</div>
+			</div>
+		`;
 
-        this.root.querySelectorAll("scoreboard__control-button").forEach(controlButton => {
-            controlButton.addEventListener("click", () => {
-                const direction = controlButton.textContent === "-" ? "minus" : "plus";
-                const player = controlButton.closest(".scoreboard__controls").dataset.forPlayer;
+		this.root.querySelectorAll(".scoreboard__control-button").forEach(controlButton => {
+			controlButton.addEventListener("click", () => {
+				const direction = controlButton.textContent === "-" ? "minus" : "plus";
+				const player = controlButton.closest(".scoreboard__controls").dataset.forPlayer;
 
-                onControlButtonClick(player, direction);
-            });
-        });
+				onControlButtonClick(player, direction);
+			});
+		});
+	}
+
+	update(playerOneScore, playerTwoScore) {
+		this.root.querySelector(".scoreboard__score[data-for-player='one']").textContent = playerOneScore;
+		this.root.querySelector(".scoreboard__score[data-for-player='two']").textContent = playerTwoScore;
+	}
+
+  
     }
-
-    update(PlayerOneScore, PlayerTwoScore) {
-        this.root.querySelector(".scoreboard__score[data-for-player='one']").textContent = PlayerOneScore;
-        this.root.querySelector(".scoreboard__score[data-for-player='two']").textContent = PlayerTwoScore;
-    }
-}
