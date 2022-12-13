caff0947afbba324e3f7912af258b34febcdfbc0
Robatortas <robatortas1@gmail.com>
2022-12-13 01:28:31 -0800

    Reset button not working dfucl

--- js/MediaButtons.js
+++ js/MediaButtons.js
@@ -3,9 +3,11 @@ const root = document.querySelector("#mediaGroup");
 var ticks = 0;
 
 var paused = false;
+var started = false;
 var isReset= false;
 
 var timer = 0;
+var milis = 0;
 
 
 console.log("MEDIABUTTONS IS ALIVE!");
@@ -16,31 +18,34 @@ let start = root.querySelectorAll(".mediaButtons").forEach(mediaButton => {
         if(mediaButton.textContent == "||") {
             console.log("PAUSE!");
             paused = !paused;
-            
         }
-
-        // PLAY
-        if(mediaButton.textContent == ">") {
-            console.log("START!");
-            setInterval(() => {
-                if(!paused) {
-                    ticks++;
-                    // console.log(this.paused)
-                    this.timer = document.getElementById("timer").textContent = ticks
-                }
-                console.log(ticks);
-            }, 1000);
-        }
-
 
         // STOP
         if(mediaButton.textContent == "↺") {
             console.log("RESET!");
-            ticks = 0;
+            ticks = -1;
             isReset = !isReset;
-            paused = !paused;
+            paused = false;
+            // if(isReset) milis = 0;
         }
         
+        // PLAY
+        if(mediaButton.textContent == ">") {
+            console.log("START!");
+            milis = 1000;
+            started = !started;
+            setInterval(() => {
+                if(!paused) {
+                     ticks++;
+                    // console.log(this.paused)
+                     this.timer = document.getElementById("timer").textContent = ticks;
+                 }
+                 if(isReset) {
+                    this.timer = 0;
+                 }
+                 console.log(ticks);
+             }, milis);
+        }
     })
 })
 
