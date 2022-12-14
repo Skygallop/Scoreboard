d495bd37c136785a1f65b5d45f54d4d47336c830
Robatortas <robatortas1@gmail.com>
2022-12-13 19:43:00 -0800

    Media Buttons WORK!

--- js/MediaButtons.js
+++ js/MediaButtons.js
@@ -2,52 +2,65 @@ const root = document.querySelector("#mediaGroup");
 
 var ticks = 0;
 
-var paused = false;
 var started = false;
+var paused = false;
 var isReset= false;
 
 var timer = 0;
 var milis = 0;
 
+var intervalId;
 
 console.log("MEDIABUTTONS IS ALIVE!");
 let start = root.querySelectorAll(".mediaButtons").forEach(mediaButton => {
     mediaButton.addEventListener("click", () => {
-       
+
         // PAUSE
         if(mediaButton.textContent == "||") {
             console.log("PAUSE!");
-            paused = !paused;
+            // if(started) {
+                paused = !paused;
+            // }
+            
         }
 
-        // STOP
+         // RESET
         if(mediaButton.textContent == "↺") {
             console.log("RESET!");
+            
+            document.getElementById("timer").textContent= 0;
+            // paused = true;
             ticks = -1;
             isReset = !isReset;
-            paused = false;
-            // if(isReset) milis = 0;
         }
+        
         
         // PLAY
         if(mediaButton.textContent == ">") {
             console.log("START!");
             milis = 1000;
-            started = !started;
-            setInterval(() => {
-                if(!paused) {
-                     ticks++;
-                    // console.log(this.paused)
-                     this.timer = document.getElementById("timer").textContent = ticks;
-                 }
-                 if(isReset) {
-                    this.timer = 0;
-                 }
-                 console.log(ticks);
-             }, milis);
+            started = true;
+            paused = false;
+            isReset = false;
         }
     })
 })
+
+// TICKER
+this.intervalId = setInterval(() => {
+    if(!paused && started && !isReset) {
+        ticks++;
+        // console.log(this.paused)
+        this.timer = document.getElementById("timer").textContent = ticks;
+    }
+    
+    console.log(ticks);
+}, 1000);
+
+
+if(isReset) {
+    
+}
 
 // pauseButton = document.querySelector("");
 
