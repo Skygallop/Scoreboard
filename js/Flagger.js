df1eb3eae34021ae17e054c09c0248dafbe23603
Robatortas <robatortas1@gmail.com>
2022-12-17 02:17:35 -0800

    FIXED REIDICULOUS AMOUNTS OF REQUESTS BUG

--- js/Flagger.js
+++ js/Flagger.js
@@ -1,5 +1,5 @@
 /*
-    SCRIPT ALGORITHM WHICH TELLS THE PROGRAM WHICH FLAG TO DISPLAY
+    SCRIPT THAT TELLS THE PROGRAM WHICH FLAG TO DISPLAY
     DEPENDING ON THE COUNTRY OF CHOICE.
 */
 const flagList = "https://raw.githubusercontent.com/Robatortas/Flags_Server/main/flag_list.flag";
@@ -7,13 +7,16 @@ const flagList = "https://raw.githubusercontent.com/Robatortas/Flags_Server/main
 let playerOneName = $("#teamOneDrop").value;
 let playerTwoName = $("#teamTwoDrop").value;
 
-setInterval(() => {
-    playerOneName = document.querySelector("#teamOneDrop").value;
-     playerTwoName = document.querySelector("#teamTwoDrop").value;
-}, 100);
+var teamOneFlag = document.getElementById("teamOneFlag").src;
+var teamTwoFlag = document.getElementById("teamTwoFlag").src;
 
-function getText() {
+async function getText() {
     var request = new XMLHttpRequest();
+    let bool = false;
+    var interval = setInterval(() => {  
+    playerOneName = document.querySelector("#teamOneDrop").value;
+    playerTwoName = document.querySelector("#teamTwoDrop").value;
+
     request.open('GET', flagList, true);
     request.send(null);
     request.onreadystatechange = () => {
@@ -24,27 +27,24 @@ function getText() {
                 for(var i = 0; i < array.length; i++) {
                     if(array[i].toString().includes(playerOneName)) {
                         document.getElementById("teamOneFlag").src = array[i].toString();
+                        clearInterval(interval);
                     }
                     if(array[i].toString().includes(playerTwoName)) {
                         document.getElementById("teamTwoFlag").src = array[i].toString();
+                        clearInterval(interval);
                     }
                 }
-                return request.responseText.split("\n");
             }
         }
     }
+    // SECURITY MEASURE
+    clearInterval(interval);
+}, 10);
 }
 
 $("#teamOneDrop").on("change", function () {
-    var interval = setInterval(() => {    
-        getText();
-    }, 1000);
+    getText();
 });
-$("#teamTwoDrop").change(function () {
-    var interval = setInterval(() => {    
-        getText();
-    }, 1000);
+$("#teamTwoDrop").on("change", function () {
+    getText();
 });
-
-
-console.log("WORK!");
